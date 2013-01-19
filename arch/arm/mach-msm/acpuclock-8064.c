20331640d10510a0ce905e3ddb213418f8fbdc6f
morfic <morfic@gmail.com>
2013-01-18 22:19:59 -0600

    This adds:
    From 2261c1b8edb01b0f23fe5b587edb708367022d38 Mon Sep 17 00:00:00 2001
    From: Tatyana Brokhman <tlinder@codeaurora.org>
    Date: Thu, 10 Jan 2013 20:01:48 -0500
    Subject: [PATCH] block: Adding ROW scheduling algorithm
    
    This patch adds the implementation of a new scheduling algorithm - ROW.
    The policy of this algorithm is to prioritize READ requests over WRITE
    as much as possible without starving the WRITE requests.
    ---
     Documentation/block/row-iosched.txt |  134 ++++++
     block/Kconfig.iosched               |   21 +
     block/Makefile                      |    1 +
     block/row-iosched.c                 |  767 +++++++++++++++++++++++++++++++++++
     include/linux/elevator.h            |    7 +
     5 files changed, 930 insertions(+)
     create mode 100644 Documentation/block/row-iosched.txt
     create mode 100644 block/row-iosched.c
    
    Also revert voltages to previous values for 384MHz, this was not tested long enough and would interfere with the schedulers and ffc being merged.

--- arch/arm/mach-msm/acpuclock-8064.c
+++ arch/arm/mach-msm/acpuclock-8064.c
@@ -107,7 +107,7 @@ static struct msm_bus_scale_pdata bus_scale_data __initdata = {
 };
 
 static struct l2_level l2_freq_tbl[] __initdata __initdata = {
-	[0]  = { {  384000, PLL_8, 0, 2, 0x00 },  925000,  925000, 1 },
+	[0]  = { {  384000, PLL_8, 0, 2, 0x00 },  950000,  950000, 1 },
 	[1]  = { {  432000, HFPLL, 2, 0, 0x20 },  950000,  950000, 2 },
 	[2]  = { {  486000, HFPLL, 2, 0, 0x24 },  950000,  950000, 2 },
 	[3]  = { {  540000, HFPLL, 2, 0, 0x28 },  950000,  950000, 2 },
