f0909797db74a12e8f211ab6ec8c564504607f1e
morfic <morfic@gmail.com>
2013-04-24 14:44:33 -0500

    Francisco is right, onlining all cores is a nice workaround for the real problem. Onlining all cores using the driver's framework. Some whitespace cleanup.

--- arch/arm/kernel/auto_hotplug.c
+++ arch/arm/kernel/auto_hotplug.c
@@ -94,19 +94,19 @@ static unsigned int min_online_cpus = 4;
 
 static int min_online_cpus_fn_set(const char *arg, const struct kernel_param *kp)
 {
-    int ret; 
-    
+    int ret;
+
     ret = param_set_int(arg, kp);
-    
+
     ///at least 1 core must run even if set value is out of range
     if ((min_online_cpus < 1) || (min_online_cpus > CPUS_AVAILABLE))
     {
         min_online_cpus = 1;
     }
-    
+
     //online all cores and offline them based on set value
     schedule_work(&hotplug_online_all_work);
-        
+
     return ret;
 }
 
@@ -115,7 +115,7 @@ static int min_online_cpus_fn_get(char *buffer, const struct kernel_param *kp)
     return param_get_int(buffer, kp);
 }
 
-static struct kernel_param_ops min_online_cpus_ops = 
+static struct kernel_param_ops min_online_cpus_ops =
 {
     .set = min_online_cpus_fn_set,
     .get = min_online_cpus_fn_get,
@@ -367,7 +367,7 @@ static void auto_hotplug_late_resume(struct early_suspend *handler)
 	pr_info("auto_hotplug: late resume handler\n");
 	flags &= ~EARLYSUSPEND_ACTIVE;
 
-	schedule_delayed_work_on(0, &hotplug_decision_work, HZ);
+	schedule_work(&hotplug_online_all_work);
 }
 
 static struct early_suspend auto_hotplug_suspend = {
